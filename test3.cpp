b46ce5b8e72a42a24682d27bced766f53b2fdbc3
rgosens2 <rgosens2@gmail.com>
2021-12-29 22:41:18 +0100

    Launch M1 and (x<=>1)

--- test3.cpp
+++ test3.cpp
@@ -47,6 +47,7 @@ int main()
     std::cout.flush();
     perf p1;
     std::string out1;
+    //std::regex re(R"(^\s*\n|\n\s*$)");
     std::regex re(R"(^\s*\n|\n\s*$)");
     try {
         out1 = std::regex_replace(test, re, "");
